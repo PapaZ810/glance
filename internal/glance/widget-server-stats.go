882a0db29a3977f474eeaae47ae8dea5861f8ecd
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2026-01-04 16:17:35 -0700

    Update widget-server-stats.go

--- internal/glance/widget-server-stats.go
+++ internal/glance/widget-server-stats.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/glanceapp/glance/pkg/sysinfo"
+	"github.com/PapaZ810/glance/pkg/sysinfo"
 )
 
 var serverStatsWidgetTemplate = mustParseTemplate("server-stats.html", "widget-base.html")
